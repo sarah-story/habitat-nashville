d23c78b3bbe64bee0415e56a0fb3ce5c072ed5ca
Sarah Story <sstory@myemma.com>
2016-03-13 18:08:16 -0500

    update address endpoints

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ $( document ).ready(function() {
         var html = "";
         $.each(events, function(i, event) {
             var remaining_tickets = event.ticket_classes[0].quantity_total - event.ticket_classes[0].quantity_sold
-            console.log(remaining_tickets);
             if (remaining_tickets > 0) {
-                html += "<div class='well'><h2>" + event.name.html + "</h2><p>" + event.venue.name + "</p><p>Remaining spots: <b>" + remaining_tickets + "</b></p><a class='btn btn-default' href='" + event.url + "' role='button' target='_blank'>Sign Up to Volunteer</a></div>";
+                html += "<div class='well'><h2>" + event.name.html + "</h2><p>" + event.venue.address.address_1 + "</p><p>Remaining spots: <b>" + remaining_tickets + "</b></p><a class='btn btn-default' href='" + event.url + "' role='button' target='_blank'>Sign Up to Volunteer</a></div>";
             }
         });
-        console.log(html);
         $("#upcoming").html(html);
     });
 });
